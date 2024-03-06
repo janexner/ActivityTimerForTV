bdbc9d6f065a5dcd5642168e87fa3aea51b93248
jexner <jan.exner@gmx.net>
2024-03-06 20:15:00 +0100

    In which our hero fixes imports

--- app/src/main/java/com/exner/tools/activitytimerfortv/ui/CustomComponents.kt
+++ app/src/main/java/com/exner/tools/activitytimerfortv/ui/CustomComponents.kt
@@ -12,7 +12,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
@@ -21,7 +20,6 @@ import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.tv.material3.ExperimentalTvMaterial3Api
 import androidx.tv.material3.LocalContentColor
-import androidx.tv.material3.LocalTextStyle
 import androidx.tv.material3.MaterialTheme
 import androidx.tv.material3.Text
 import com.exner.tools.activitytimerfortv.ui.tools.AutoSizeText
