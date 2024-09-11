048d9042d78bf77dc12e5376e1cba3e17912aec1
Jan Exner <jexner@adobe.com>
2024-09-11 23:36:47 +0200

    in which our hero makes small UI changes, mainly Buttons

--- app/src/main/java/com/exner/tools/activitytimerfortv/ui/destination/ProcessDetails.kt
+++ app/src/main/java/com/exner/tools/activitytimerfortv/ui/destination/ProcessDetails.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Call
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.PlayArrow
@@ -18,13 +19,17 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.tv.material3.Button
+import androidx.tv.material3.ButtonDefaults
 import androidx.tv.material3.Icon
+import androidx.tv.material3.OutlinedButton
 import androidx.tv.material3.Text
 import androidx.tv.material3.WideButton
 import com.exner.tools.activitytimerfortv.ui.ProcessDetailsViewModel
 import com.exner.tools.activitytimerfortv.ui.TimerDisplay
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootGraph
+import com.ramcosta.composedestinations.generated.destinations.ImportFromNearbyDeviceDestination
 import com.ramcosta.composedestinations.generated.destinations.ProcessDeleteDestination
 import com.ramcosta.composedestinations.generated.destinations.ProcessRunDestination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
@@ -57,46 +62,69 @@ fun ProcessDetails(
     ) {
         // buttons
         Row {
-            WideButton(
+            Button(
                 onClick = {
                     navigator.navigate(
                         ProcessRunDestination(processUuid = processUuid)
                     )
                 },
-                title = { Text(text = "Start Process") },
-                icon = {
-                    Icon(
-                        imageVector = Icons.Filled.PlayArrow,
-                        contentDescription = "Start Process"
-                    )
-                }
-            )
-            Spacer(modifier = Modifier.size(8.dp))
-            WideButton(
+                contentPadding = ButtonDefaults.ButtonWithIconContentPadding
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.PlayArrow,
+                    contentDescription = "Start Process",
+                    modifier = Modifier.size(ButtonDefaults.IconSize)
+                )
+                Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
+                Text(text = "Start Process")
+            }
+            Spacer(modifier = Modifier.size(16.dp))
+            OutlinedButton(
                 onClick = {
 //                        navigator.navigate(ProcessEditDestination(processUuid = processUuid))
                 },
-                title = { Text(text = "Edit Process") },
-                icon = {
-                    Icon(
-                        imageVector = Icons.Filled.Edit,
-                        contentDescription = "Edit Process"
-                    )
-                }
-            )
+                contentPadding = ButtonDefaults.ButtonWithIconContentPadding
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Edit,
+                    contentDescription = "Edit Process",
+                    modifier = Modifier.size(ButtonDefaults.IconSize)
+                )
+                Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
+                Text(text = "Edit Process")
+            }
+
+            // temp
+            Spacer(modifier = Modifier.size(16.dp))
+            OutlinedButton(
+                onClick = {
+                    navigator.navigate(ImportFromNearbyDeviceDestination)
+                },
+                contentPadding = ButtonDefaults.ButtonWithIconContentPadding,
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Call,
+                    contentDescription = "Import from Nearby Device",
+                    modifier = Modifier.size(ButtonDefaults.IconSize)
+                )
+                Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
+                Text(text = "Receive Processes")
+            }
+            // temp end
             Spacer(modifier = Modifier.weight(0.5f))
-            WideButton(
+            OutlinedButton(
                 onClick = {
                     navigator.navigate(ProcessDeleteDestination(processUuid = processUuid))
                 },
-                title = { Text(text = "Delete Process") },
-                icon = {
-                    Icon(
-                        imageVector = Icons.Filled.Delete,
-                        contentDescription = "Delete Process"
-                    )
-                }
-            )
+                contentPadding = ButtonDefaults.ButtonWithIconContentPadding,
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Delete,
+                    contentDescription = "Delete Process"
+                )
+                Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
+                Text(text = "Delete Process")
+            }
         }
         // spacer
         Spacer(modifier = Modifier.weight(0.1f))
